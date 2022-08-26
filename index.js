b6c6ce7f6b1012c48a698d7c79674a0dc4ac0224
danielpdaniel <artofjquinn@gmail.com>
2022-08-26 15:23:11 -0400

    commit tree a48f6b4359bdf55d5e3dccdf4138d71f27c67dea

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const { stringifyRequest } = require("nock/lib/common")
+
 // Add your code here
+function submitData(name, email){
+    const postObject = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        })
+    }
+return fetch("http://localhost:3000/users", postObject)
+.then(response => response.json())
+.then(object => {
+    let objId = document.createElement("div");
+    objId.className = "idNumber";
+    objId.textContent = object.id;
+    document.body.appendChild(objId);
+    console.log(objId); 
+    })
+.catch(function(error){
+    let errorMessage = document.createElement("div");
+    errorMessage.className = "error";
+    errorMessage.textContent = `Unauthorized Access: ${error.message},`;
+    document.body.appendChild(errorMessage);
+})
+}
